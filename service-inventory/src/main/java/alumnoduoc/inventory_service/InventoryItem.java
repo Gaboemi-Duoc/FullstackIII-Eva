ae4f5ba9f43753eaa57935f6e3cd5a22c9faaab6
isak180 <is.chacana@duocuc.cl>
2026-05-11 21:24:46 -0400

    service-inventory(finalizado)
    proyecto springboot corre , falta probar database h2 y endpoints

--- service-inventory/src/main/java/alumnoduoc/inventory_service/InventoryItem.java
+++ service-inventory/src/main/java/alumnoduoc/inventory_service/InventoryItem.java
@@ -1,35 +1,28 @@
 package alumnoduoc.inventory_service;
 
-import jakartInventoryItem.*;
+import jakarta.persistence.*;
 import lombok.*;
-
-// Igual que User.java pero para productos del inventario.
-// @Table(name = "inventory_item") evita conflictos con palabras reservadas SQL.
 
 @Entity
 @Table(name = "inventory_item")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class inventoryItem {
+public class InventoryItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_item;
 
-    // Nombre del producto (ej: "Tornillo M8")
     @Column(nullable = false)
     private String nombre;
 
-    // Descripción breve del producto
     @Column(nullable = false)
     private String descripcion;
 
-    // Stock disponible en bodega
     @Column(nullable = false)
     private Integer cantidad;
 
-    // Precio unitario
     @Column(nullable = false)
     private Double precio;
 }
