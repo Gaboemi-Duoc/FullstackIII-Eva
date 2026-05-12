1a8d393e34c5e1233fea1fa2f7aaced9c6b2a714
Gaboemi-Duoc <173064947+Gaboemi-Duoc@users.noreply.github.com>
2026-05-12 10:39:32 -0400

    URL como var

--- bff/src/main/java/alumnoduoc/bff/client/UserServiceClient.java
+++ bff/src/main/java/alumnoduoc/bff/client/UserServiceClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "user-service", url = "http://localhost:9090") 
+@FeignClient(name = "user-service", url = "${user-service.url}") 
 public interface UserServiceClient {
     
     @GetMapping("/api/users")
