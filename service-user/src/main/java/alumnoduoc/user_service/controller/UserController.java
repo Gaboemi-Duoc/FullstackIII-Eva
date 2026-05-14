7adab6c5664b872eb41407a456c9af0132bd6ee9
Vicente Palacios <vi.palaciosp@duocuc.cl>
2026-05-13 20:01:32 -0400

    pequeño arreglo

--- service-user/src/main/java/alumnoduoc/user_service/controller/UserController.java
+++ service-user/src/main/java/alumnoduoc/user_service/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*")
 public class UserController {
     private final UserService userService;
 
