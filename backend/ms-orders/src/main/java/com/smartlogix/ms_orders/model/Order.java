cd18095f1193acd3129d49e5d6cb02196c38eae9
Vicente Palacios <vi.palaciosp@duocuc.cl>
2026-06-16 18:40:04 -0400

    ms orders creado, sin dcoker, sin db, sin k8s

---
+++ backend/ms-orders/src/main/java/com/smartlogix/ms_orders/model/Order.java
@@ -0,0 +1,37 @@
+package com.smartlogix.ms_orders.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_order;
+
+    @Column(nullable = false)
+    private String customerName;
+
+    @Column(nullable = false)
+    private String customerEmail;
+
+    @Column(nullable = false, length = 500)
+    private String deliveryAddress;
+
+    @Column(nullable = false)
+    private Double total;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
---
+++ backend/ms-orders/src/main/java/com/smartlogix/ms_orders/model/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.smartlogix.ms_orders.model;
+
+public enum OrderStatus {
+    CREATED,
+    CONFIRMED,
+    IN_PREPARATION,
+    DISPATCHED,
+    DELIVERED,
+    CANCELLED
+}
