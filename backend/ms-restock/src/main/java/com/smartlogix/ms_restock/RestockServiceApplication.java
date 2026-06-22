98c94bfe9a774dcda87cd58cf96cb1c94f707ac1
isak180 <is.chacana@duocuc.cl>
2026-06-21 23:29:36 -0400

    (ms-restock): corregir pom.xml, Dockerfile y habilitar Flyway

--- backend/ms-restock/src/main/java/com/smartlogix/ms_restock/RestockServiceApplication.java
+++ backend/ms-restock/src/main/java/com/smartlogix/ms_restock/RestockServiceApplication.java
@@ -2,8 +2,10 @@ package com.smartlogix.ms_restock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RestockServiceApplication {
 
     public static void main(String[] args) {
