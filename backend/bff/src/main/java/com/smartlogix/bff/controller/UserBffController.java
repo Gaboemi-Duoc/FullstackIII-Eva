5a4fcb554ec93405e95af7ae139a2544dd553ba9
isak180 <is.chacana@duocuc.cl>
2026-07-06 21:20:15 -0400

    .

--- backend/bff/src/main/java/com/smartlogix/bff/controller/UserBffController.java
+++ backend/bff/src/main/java/com/smartlogix/bff/controller/UserBffController.java
@@ -20,7 +20,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+ 
 @Slf4j
 @RestController
 @RequestMapping("/api/bff/users")
